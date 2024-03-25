367c6f7837c15abf407eae756dc03264553122ad
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-03-25 10:26:38 -0700

    basic implementation of a custom 'devise lockable' configuration that only locks teachers

--- dashboard/config/initializers/devise.rb
+++ dashboard/config/initializers/devise.rb
@@ -199,24 +199,24 @@ Devise.setup do |config|
   # Defines which strategy will be used to lock an account.
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
+  config.lock_strategy = :failed_attempts
 
   # Defines which key will be used when locking and unlocking an account
-  # config.unlock_keys = [ :email ]
+  config.unlock_keys = [:email]
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user email
   # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
-  # config.unlock_strategy = :both
+  config.unlock_strategy = :both
 
   # Number of authentication tries before locking an account if lock_strategy
   # is failed attempts.
-  # config.maximum_attempts = 20
+  config.maximum_attempts = 4
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  # config.unlock_in = 1.hour
+  config.unlock_in = 1.hour
 
   # ==> Configuration for :recoverable
   #
@@ -337,9 +337,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
-  require 'custom_devise_failure'
+  require 'devise/custom_failure'
   config.warden do |manager|
-    manager.failure_app = CustomDeviseFailure
+    manager.failure_app = Devise::CustomFailure
   end
 
   require 'cdo/cookie_helpers'
@@ -390,3 +390,8 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 end
+
+Devise.add_module(
+  :custom_lockable,
+  model: Rails.root.join('lib', 'devise', 'custom_lockable')
+)
